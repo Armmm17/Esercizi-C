5fd34bdb86deee69da9afe757ede17322d905227
Armmm17 <55357189+Armmm17@users.noreply.github.com>
2022-04-04 15:36:05 +0200

    fix

--- ES SCUOLA/ES 06) Input voti in array, determina maggiore, minore e media/main.c
+++ ES SCUOLA/ES 06) Input voti in array, determina maggiore, minore e media/main.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 int main(void) {
-    int i, voto, maggiore, minore, nVoti, somma;
-    float media;
-    printf("Di qunati studenti vuoi inserire i voti? ");
+    int i, maggiore, minore, nVoti;
+    float media, voto, somma;
+    printf("Di quanti studenti vuoi inserire i voti? ");
     scanf("%d", &nVoti);
-    int voti[nVoti];
+    float voti[nVoti];
 
     // Riempimento array con i voti e controllo se i voti sono minori-uguali a 10
     for (i = 0; i < nVoti; i++) {
         printf(" Inserisci il voto: ");
-        scanf("%d", &voto);
+        scanf("%f", &voto);
         for(;voto>10;){
             printf("Il voto inserito non e' valido (maggiore di 10), inserisci nuovamente il voto: ");
-            scanf("%d", &voto);
+            scanf("%f", &voto);
         }
         voti[i] = voto;
     }
 
     // DEBUG Stampa tutti i voti contenuti nell'array
     for (i = 0; i < nVoti; i++) {
-        printf("\nIl voto nella posizione %d e' %d", i, voti[i]);
+        printf("\nIl voto nella posizione %d e' %f", i, voti[i]);
     }
 
     maggiore = voti[0];
@@ -39,7 +39,7 @@ int main(void) {
     for (i = 0; i < nVoti; i++) {
         somma += voti[i];
     }
-    printf("\nSomma dei voti: %d\n", somma); //DEBUG per controllare la somma
+    printf("\nSomma dei voti: %f\n", somma); //DEBUG per controllare la somma
     media = (float)somma / (float)nVoti;
     printf("La media dei voti inseriti e': %f", media);
 }
--- ES SCUOLA/ES 07) Riempi array e stampa al contrario/main.c
+++ ES SCUOLA/ES 07) Riempi array e stampa al contrario/main.c
@@ -1,33 +1,26 @@
 #include <stdio.h>
 
 int main(void) {
-    int i, lenArr, numero;
+    int i, lenArr;
     printf("Quanti numeri vuoi inserire? ");
-    lenArr = 0;
     scanf(" %d", &lenArr);
     int numeri[lenArr];
 
     // Riempi array
-    i = 0;
     for (i = 0; i < lenArr; i++) {
         printf("Inserisci il numero: ");
-        scanf("%d", &numero);
-        numeri[i] = numero;
+        scanf("%d", &numeri[i]);
     }
 
-    /*DEBUG CARICAMENTO LISTA
-    for (i = 0; i < lenArr; i++) {
-        printf("\nIl numero nella posizione %d e' %d", i, numeri[i]);
-    }
-    */
     //Stampa array al normale
     for (i = 0; i < lenArr; i++) {
         printf("%d ", numeri[i]);
     }
 
+    printf("\n");
+
     //Stampa array al contrario
     for (i = lenArr-1; i >= 0 ; i--) {
         printf("%d ", numeri[i]);
     }
-
 }
